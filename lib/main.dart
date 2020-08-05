27c640ae15a8b689da38bac92e09d946925cf62a
Vincent Wong <vincent.wong@upcentral.com>
2020-08-05 14:08:06 +0800

    use html element view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +19,8 @@ class TextFieldWebDemo extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Text Field Web Demo (Press Button to add 20 rows)'),
+      home: MyHomePage(
+          title: 'Text Field Web Demo (Press Button to add 20 rows)'),
     );
   }
 }
@@ -32,7 +35,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  String viewID = "test-html";
   int numOfRows = 20;
+
+  @override
+  void initState() {
+    super.initState();
+    ui.platformViewRegistry.registerViewFactory(
+      viewID,
+      (int id) => IFrameElement()
+          ..style.border = 'none'
+          ..src = '/assets/test.html');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,70 +56,69 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: Scrollbar(
-            child: ListView.builder(
-          itemCount: numOfRows,
-          itemBuilder: (BuildContext context, int index) {
-            if (index % 5 == 0)
-              return Divider(
-                height: 20,
-                thickness: 2,
-                color: Colors.red,
-              );
-            return Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 24),
-                    child: Container(
-                        width: 50,
-                        child: Text(
-                          index.toString(),
-                          textAlign: TextAlign.center,
-                        ))),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(), labelText: 'Test1'),
-                      maxLines: null,
-                    ),
-                  )
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(), labelText: 'Test2'),
-                      maxLines: null,
-                    ),
-                  )
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(), labelText: 'Test3'),
-                      maxLines: null,
-                    ),
-                  )
-                ),
-              ],
-            );
-          },
-        )),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              numOfRows = numOfRows + 20;
-            });
-          },
-          child: Icon(
-            Icons.add
-          )
-        )
+          child: HtmlElementView(
+                viewType: 'test-html',
+              ),
+        ),
       );
+        // body: Scrollbar(
+        //     child: ListView.builder(
+        //   itemCount: numOfRows,
+        //   itemBuilder: (BuildContext context, int index) {
+        //     if (index % 5 == 0)
+        //       return Divider(
+        //         height: 20,
+        //         thickness: 2,
+        //         color: Colors.red,
+        //       );
+        //     return Row(
+        //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //       children: [
+        //         Padding(
+        //             padding: const EdgeInsets.symmetric(horizontal: 24),
+        //             child: Container(
+        //                 width: 50,
+        //                 child: Text(
+        //                   index.toString(),
+        //                   textAlign: TextAlign.center,
+        //                 ))),
+        //         Expanded(
+        //             child: Padding(
+        //           padding: const EdgeInsets.symmetric(horizontal: 12),
+        //           child: TextField(
+        //             decoration: InputDecoration(
+        //                 border: OutlineInputBorder(), labelText: 'Test1'),
+        //             maxLines: null,
+        //           ),
+        //         )),
+        //         Expanded(
+        //             child: Padding(
+        //           padding: const EdgeInsets.symmetric(horizontal: 12),
+        //           child: TextField(
+        //             decoration: InputDecoration(
+        //                 border: OutlineInputBorder(), labelText: 'Test2'),
+        //             maxLines: null,
+        //           ),
+        //         )),
+        //         Expanded(
+        //             child: Padding(
+        //           padding: const EdgeInsets.symmetric(horizontal: 12),
+        //           child: TextField(
+        //             decoration: InputDecoration(
+        //                 border: OutlineInputBorder(), labelText: 'Test3'),
+        //             maxLines: null,
+        //           ),
+        //         )),
+        //       ],
+        //     );
+        //   },
+        // )),
+        // floatingActionButton: FloatingActionButton(
+        //     onPressed: () {
+        //       setState(() {
+        //         numOfRows = numOfRows + 20;
+        //       });
+        //     },
+        //     child: Icon(Icons.add)));
   }
 }
